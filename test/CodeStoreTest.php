90dbfd3df11aa70972c97898e7dad849538f7a05
Paul Water <p.r.water@setbased.nl>
2016-06-25 15:16:34 +0200

    Added unit tests.

---
+++ test/CodeStoreTest.php
@@ -0,0 +1,79 @@
+<?php
+//----------------------------------------------------------------------------------------------------------------------
+namespace SetBased\Helper\CodeStore\Test;
+
+use SetBased\Helper\CodeStore\PhpCodeStore;
+
+//----------------------------------------------------------------------------------------------------------------------
+class PhpCodeStoreTest extends \PHPUnit_Framework_TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test indentation levels for different coding styles.
+   */
+  public function testIndentationLevel()
+  {
+    $store = new PhpCodeStore(2, 80);
+
+    $store->append('<?php');
+    $store->appendSeparator();
+    $store->append('namespace SetBased\Helper\CodeStore\Test;');
+    $store->append('');
+    $store->append('use SetBased\Helper\CodeStore\PhpCodeStore;');
+    $store->append('');
+    $store->appendSeparator();
+    $store->append('class PhpCodeStoreTest extends \PHPUnit_Framework_TestCase');
+    $store->append('{');
+    $store->appendSeparator();
+    $store->append('public function testIndentationLevel()');
+    $store->append('{');
+    $store->append('// Mix some coding styles.');
+    $store->append('if (true) {');
+    $store->append('echo "true";');
+    $store->append('} else {');
+    $store->append('echo "false";');
+    $store->append('}');
+    $store->append('}');
+    $store->append('');
+    $store->appendSeparator();
+    $store->append('}');
+    $store->append('');
+    $store->appendSeparator();
+
+    $expected = <<< EOL
+<?php
+//------------------------------------------------------------------------------
+namespace SetBased\Helper\CodeStore\Test;
+
+use SetBased\Helper\CodeStore\PhpCodeStore;
+
+//------------------------------------------------------------------------------
+class PhpCodeStoreTest extends \PHPUnit_Framework_TestCase
+{
+  //----------------------------------------------------------------------------
+  public function testIndentationLevel()
+  {
+    // Mix some coding styles.
+    if (true) {
+      echo "true";
+    } else {
+      echo "false";
+    }
+  }
+
+  //----------------------------------------------------------------------------
+}
+
+//------------------------------------------------------------------------------
+
+EOL;
+
+    $code = $store->getCode();
+
+    $this->assertEquals($expected, $code);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
