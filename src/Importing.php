6334ba88eb0e10e060a779961354aa5f7ce46d3b
Paul Water <p.r.water@setbased.nl>
2020-04-24 18:09:17 +0200

    Add class \SetBased\Helper\CodeStore\Importing.

---
+++ src/Importing.php
@@ -0,0 +1,283 @@
+<?php
+declare(strict_types=1);
+
+namespace SetBased\Helper\CodeStore;
+
+/**
+ * Helper class for generating statements for importing classes.
+ */
+class Importing
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The fully qualified names to import.
+   *
+   * @var array
+   */
+  private $classes = [];
+
+  /**
+   * The import statements.
+   *
+   * @var string[]
+   */
+  private $imports;
+
+  /**
+   * The namespace.
+   *
+   * @var string
+   */
+  private $namespace;
+
+  /**
+   * The replace pairs from fully qualified name to imported name.
+   *
+   * @var array
+   */
+  private $replace;
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Object constructor.
+   *
+   * @param string $namespace The namespace.
+   */
+  public function __construct(string $namespace)
+  {
+    $this->namespace = self::fullyQualify($namespace);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * The (fully) qualified name optionally without leading slash.
+   *
+   * @param string $name The fully qualified name with lead slash.
+   *
+   * @return string
+   */
+  public static function fullyQualify(string $name): string
+  {
+    return '\\'.ltrim($name, '\\');
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test for name collision.
+   *
+   * @param array  $rawImports         The (raw) uses data.
+   * @param string $fullyQualifiedName The fully qualified class name.
+   *
+   * @return bool
+   */
+  private static function collision1(array $rawImports, string $fullyQualifiedName): bool
+  {
+    [, $name] = self::split($fullyQualifiedName);
+
+    foreach ($rawImports as $rawImport)
+    {
+      if ($rawImport['fully_qualified_name']!==$fullyQualifiedName && $rawImport['name']===$name)
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test for collision on alias or name.
+   *
+   * @param array  $rawImports         The (raw) uses data.
+   * @param string $fullyQualifiedName The fully qualified class name.
+   * @param string $alias              The alias.
+   *
+   * @return bool
+   */
+  private static function collision2(array $rawImports, string $fullyQualifiedName, string $alias): bool
+  {
+    foreach ($rawImports as $rawImport)
+    {
+      if ($rawImport['fully_qualified_name']!==$fullyQualifiedName &&
+        ($rawImport['name']===$alias || $rawImport['alias']===$alias))
+      {
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Splits a fully qualified name into a namespace and class name.
+   *
+   * @param string $name The fully qualified name.
+   *
+   * @return array
+   */
+  private static function split(string $name): array
+  {
+    $parts = explode('\\', self::fullyQualify($name));
+    $name  = array_pop($parts);
+
+    return [implode('\\', $parts), $name];
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Add a class to import.
+   *
+   * @param string $class The fully qualified name of the class.
+   */
+  public function addClass(string $class): void
+  {
+    $this->classes[] = self::fullyQualify($class);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the import statements.
+   *
+   * @return string[]
+   */
+  public function imports(): array
+  {
+    return $this->imports;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Prepares the data for retrieving import statements and replace pairs (see methods replacePairs() and imports().
+   */
+  public function prepare(): void
+  {
+    $rawUses = $this->prepare0();
+    $this->prepare1($rawUses);
+    $this->prepare2($rawUses);
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the replace pairs from fully qualified name to imported name.
+   *
+   * @return array
+   */
+  public function replacePairs(): array
+  {
+    return $this->replace;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns the imported name given a fully qualified name.
+   *
+   * @param string $fullyQualifiedName The fully qualified name.
+   *
+   * @return string
+   */
+  public function simplyFullyQualifiedName(string $fullyQualifiedName): string
+  {
+    return $this->replace[self::fullyQualify($fullyQualifiedName)];
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Returns raw data about classes to import.
+   *
+   * @return array
+   */
+  private function prepare0(): array
+  {
+    $this->classes = array_unique($this->classes);
+    sort($this->classes);
+
+    $rawImports = [];
+    foreach ($this->classes as $fullyQualifiedName)
+    {
+      [$namespace, $name] = self::split($fullyQualifiedName);
+
+      $rawImports[] = ['fully_qualified_name' => $fullyQualifiedName,
+                       'namespace'            => $namespace,
+                       'name'                 => $name,
+                       'alias'                => null,
+                       'import'               => ($namespace!==$this->namespace)];
+    }
+
+    foreach ($rawImports as &$rawImport)
+    {
+      if (self::collision1($rawImports, $rawImport['fully_qualified_name']))
+      {
+        $i = 1;
+        do
+        {
+          $alias = sprintf('%s%s%d', $rawImport['name'], 'Alias', $i);
+          $i++;
+        } while (self::collision2($rawImports, $this->namespace, $alias));
+
+        $rawImport['alias'] = $alias;
+      }
+    }
+
+    return $rawImports;
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Prepares the import statements.
+   *
+   * @param array $rawImports The raw data about classes to import.
+   */
+  private function prepare1(array $rawImports): void
+  {
+    $this->imports = [];
+    foreach ($rawImports as $rawImport)
+    {
+      if ($rawImport['import'])
+      {
+        if ($rawImport['alias']===null)
+        {
+          $this->imports[] = sprintf('use %s;', ltrim($rawImport['fully_qualified_name'], '\\'));
+        }
+        else
+        {
+          $this->imports[] = sprintf('use %s as %s;', ltrim($rawImport['fully_qualified_name'], '\\'), $rawImport['alias']);
+        }
+      }
+    }
+
+    usort($this->imports, function (string $a, string $b) {
+      return str_replace('\\', ' ', $a)<=>str_replace('\\', ' ', $b);
+    });
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Prepares the replace pairs.
+   *
+   * @param array $rawImports The raw data about classes to import.
+   */
+  private function prepare2(array $rawImports): void
+  {
+    usort($rawImports, function (array $a, array $b) {
+      $cmp = (strlen($b['fully_qualified_name'])<=>strlen($a['fully_qualified_name']));
+      if ($cmp==0)
+      {
+        $cmp = ($a['fully_qualified_name']<=>$b['fully_qualified_name']);
+      }
+
+      return $cmp;
+    });
+
+    $this->replace = [];
+    foreach ($rawImports as $rawImport)
+    {
+      $this->replace[$rawImport['fully_qualified_name']] = $rawImport['alias'] ?? $rawImport['name'];
+    }
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
---
+++ test/ImportingTest.php
@@ -0,0 +1,161 @@
+<?php
+declare(strict_types=1);
+
+namespace SetBased\Helper\CodeStore\Test;
+
+use PHPUnit\Framework\TestCase;
+use SetBased\Helper\CodeStore\Importing;
+
+/**
+ * Test cases for Importing.
+ */
+class ImportingTest extends TestCase
+{
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test with distinct classes and collisions.
+   */
+  public function testCollision1(): void
+  {
+    $importing = new Importing(__NAMESPACE__);
+    $importing->addClass('\\Foo\\Bar\\ClassOne');
+    $importing->addClass('\\Foo\\Bar\\ClassTwo');
+    $importing->addClass('\\Bar\\Foo\\ClassTwo');
+    $importing->addClass('\\Foo\\Bar\\ClassThree');
+
+    $importing->prepare();
+
+    $expected = ['use Bar\\Foo\\ClassTwo as ClassTwoAlias1;',
+                 'use Foo\\Bar\\ClassOne;',
+                 'use Foo\\Bar\\ClassThree;',
+                 'use Foo\\Bar\\ClassTwo as ClassTwoAlias2;'];
+
+    self::assertSame($expected, $importing->imports());
+
+    $expected = ['\\Foo\\Bar\\ClassThree' => 'ClassThree',
+                 '\\Bar\\Foo\\ClassTwo'   => 'ClassTwoAlias1',
+                 '\\Foo\\Bar\\ClassOne'   => 'ClassOne',
+                 '\\Foo\\Bar\\ClassTwo'   => 'ClassTwoAlias2'];
+
+    self::assertSame($expected, $importing->replacePairs());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test with distinct classes and colliding collisions.
+   */
+  public function testCollision2(): void
+  {
+    $importing = new Importing(__NAMESPACE__);
+    $importing->addClass('\\Foo\\Bar\\ClassOne');
+    $importing->addClass('\\Foo\\Bar\\ClassTwo');
+    $importing->addClass('\\Bar\\Foo\\ClassTwo');
+    $importing->addClass('\\Foo\\Bar\\ClassTwoAlias1');
+    $importing->addClass('\\Foo\\Bar\\ClassTwoAlias2');
+    $importing->addClass('\\Foo\\Bar\\ClassThree');
+
+    $importing->prepare();
+
+    $expected = ['use Bar\\Foo\\ClassTwo as ClassTwoAlias3;',
+                 'use Foo\\Bar\\ClassOne;',
+                 'use Foo\\Bar\\ClassThree;',
+                 'use Foo\\Bar\\ClassTwo as ClassTwoAlias4;',
+                 'use Foo\\Bar\\ClassTwoAlias1;',
+                 'use Foo\\Bar\\ClassTwoAlias2;'];
+
+    self::assertSame($expected, $importing->imports());
+
+    $expected = ['\\Foo\\Bar\\ClassTwoAlias1' => 'ClassTwoAlias1',
+                 '\\Foo\\Bar\\ClassTwoAlias2' => 'ClassTwoAlias2',
+                 '\\Foo\\Bar\\ClassThree'     => 'ClassThree',
+                 '\\Bar\\Foo\\ClassTwo'       => 'ClassTwoAlias3',
+                 '\\Foo\\Bar\\ClassOne'       => 'ClassOne',
+                 '\\Foo\\Bar\\ClassTwo'       => 'ClassTwoAlias4'];
+
+    self::assertSame($expected, $importing->replacePairs());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test with distinct classes, colliding collisions, and import from same namespace.
+   */
+  public function testCollision3(): void
+  {
+    $importing = new Importing(__NAMESPACE__);
+    $importing->addClass('\\Foo\\Bar\\ClassOne');
+    $importing->addClass('\\Foo\\Bar\\ClassTwo');
+    $importing->addClass('\\Bar\\Foo\\ClassTwo');
+    $importing->addClass(__NAMESPACE__.'\\ClassTwo');
+    $importing->addClass('\\Foo\\Bar\\ClassTwoAlias1');
+    $importing->addClass('\\Foo\\Bar\\ClassTwoAlias2');
+    $importing->addClass('\\Foo\\Bar\\ClassThree');
+
+    $importing->prepare();
+
+    $expected = ['use Bar\\Foo\\ClassTwo as ClassTwoAlias3;',
+                 'use Foo\\Bar\\ClassOne;',
+                 'use Foo\\Bar\\ClassThree;',
+                 'use Foo\\Bar\\ClassTwo as ClassTwoAlias4;',
+                 'use Foo\\Bar\\ClassTwoAlias1;',
+                 'use Foo\\Bar\\ClassTwoAlias2;'];
+
+    self::assertSame($expected, $importing->imports());
+
+    $expected = ['\\'.__NAMESPACE__.'\\ClassTwo' => 'ClassTwoAlias5',
+                 '\\Foo\\Bar\\ClassTwoAlias1'    => 'ClassTwoAlias1',
+                 '\\Foo\\Bar\\ClassTwoAlias2'    => 'ClassTwoAlias2',
+                 '\\Foo\\Bar\\ClassThree'        => 'ClassThree',
+                 '\\Bar\\Foo\\ClassTwo'          => 'ClassTwoAlias3',
+                 '\\Foo\\Bar\\ClassOne'          => 'ClassOne',
+                 '\\Foo\\Bar\\ClassTwo'          => 'ClassTwoAlias4'];
+
+    self::assertSame($expected, $importing->replacePairs());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Test with no imports.
+   */
+  public function testNoImports(): void
+  {
+    $importing = new Importing(__NAMESPACE__);
+
+    $importing->prepare();
+
+    self::assertSame([], $importing->imports());
+    self::assertSame([], $importing->replacePairs());
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  /**
+   * Simple test with 3 distinct classes.
+   */
+  public function testSimpleCase(): void
+  {
+    $importing = new Importing(__NAMESPACE__);
+    $importing->addClass('\\Foo\\Bar\\ClassOne');
+    $importing->addClass('Foo\\Bar\\ClassTwo');
+    $importing->addClass('\\Foo\\Bar\\ClassThree');
+
+    $importing->prepare();
+
+    $expected = ['use Foo\\Bar\\ClassOne;',
+                 'use Foo\\Bar\\ClassThree;',
+                 'use Foo\\Bar\\ClassTwo;'];
+
+    self::assertSame($expected, $importing->imports());
+
+    $expected = ['\\Foo\\Bar\\ClassThree' => 'ClassThree',
+                 '\\Foo\\Bar\\ClassOne'   => 'ClassOne',
+                 '\\Foo\\Bar\\ClassTwo'   => 'ClassTwo'];
+
+    self::assertSame($expected, $importing->replacePairs());
+
+    self::assertSame('ClassThree', $importing->simplyFullyQualifiedName('Foo\\Bar\\ClassThree'));
+    self::assertSame('ClassThree', $importing->simplyFullyQualifiedName('\\Foo\\Bar\\ClassThree'));
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+}
+
+//----------------------------------------------------------------------------------------------------------------------
